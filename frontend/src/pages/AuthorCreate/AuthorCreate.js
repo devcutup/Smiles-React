f33ac29d52abc94fa52bc1d540a4a6d66e23ccd6
gurujustin <italexx.ua@gmail.com>
2021-03-07 07:48:09 -0600

    initial project with smile

---
+++ frontend/src/pages/AuthorCreate/AuthorCreate.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { ROUTE_AUTHOR_LIST } from '../../constants';
+import { createAuthor } from '../../services/authors';
+
+function AuthorCreate() {
+    const history = useHistory();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handleSave = async () => {
+        const payload = { firstName, lastName };
+        await createAuthor(payload);
+        history.push(ROUTE_AUTHOR_LIST);
+    };
+
+    return (
+        <div className="AuthorCreate">
+            <h1>Create Author</h1>
+            <Form>
+                <Form.Group>
+                    <Form.Label>FirstName</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="FirsName"
+                        value={ firstName }
+                        onChange={ (event) => setFirstName(event.target.value) }
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>LastName</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="LastName"
+                        value={ lastName }
+                        onChange={ (event) => setLastName(event.target.value) }
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={ handleSave }>
+                    Save Author
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+export default AuthorCreate;
---
+++ frontend/src/pages/AuthorEdit/AuthorEdit.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { ROUTE_AUTHOR_LIST } from '../../constants';
+import { getAuthor, editAuthor} from '../../services/authors';
+
+function AuthorEdit(props) {
+    const history = useHistory();
+    const { authorId } = useParams();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    useEffect(() => {
+        const fetchAuthor = async () => {
+            const data = await getAuthor(authorId);
+            setFirstName(data.firstName);
+            setLastName(data.lastName);
+        };
+
+        fetchAuthor();
+    }, [authorId]);
+
+    const handleSave = async () => {
+        const payload = { firstName, lastName };
+        await editAuthor(authorId, payload);
+        history.push(ROUTE_AUTHOR_LIST);
+    };
+
+    return (
+        <div className="AuthorEdit">
+            <h1>Edit Author</h1>
+            <Form>
+                <Form.Group>
+                    <Form.Label>FirstName</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="FirstName"
+                        value={ firstName }
+                        onChange={ (event) => setFirstName(event.target.value) }
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>LastName</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="LastName"
+                        value={ lastName }
+                        onChange={ (event) => setLastName(event.target.value) }
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={ handleSave }>
+                    Save Author
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+export default AuthorEdit;
